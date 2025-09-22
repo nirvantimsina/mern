25add23e608c75a9b786735b7649016808d76bcb
Nirvan Timsina <somnathtimsina5@gmail.com>
2025-09-22 11:15:23 +0545

    tailwind and react setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Route, Routes } from "react-router";
+import Home from "./components/Home.jsx";
+import Create from "./components/Create.jsx";
+import Detail from "./components/Detail.jsx";
+import toast from "react-hot-toast";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <button
+        onClick={() => toast.success("Congrats")}
+        className="text-red-300 border-1 rounded-2xl text-bold text-2xl p-6 m-4"
+      >
+        Click Me!
+      </button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/note/:id" element={<Detail />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
